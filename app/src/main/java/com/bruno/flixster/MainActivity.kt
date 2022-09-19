28657616a73ddd5b275bd3255ce1cc6963d95a6a
Bruno Mota <bruno.ed.mota@gmail.com>
2022-09-19 18:30:25 -0400

    Parsed the Json and used Glide for posters

--- app/src/main/java/com/bruno/flixster/MainActivity.kt
+++ app/src/main/java/com/bruno/flixster/MainActivity.kt
@@ -1,21 +1,38 @@
 package com.bruno.flixster
 
-import android.nfc.Tag
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bruno.flixster.Data.Movie
+import com.bruno.flixster.UI.MovieAdapter
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
+
+
 private const val TAG = "MainActivity"
 private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+private val movies = mutableListOf<Movie>()
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvMovies: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rvMovies = findViewById(R.id.recyclerView)
+
+
+
+        val movieAdapter = MovieAdapter(this,movies)
+        rvMovies.adapter = movieAdapter
+        rvMovies.layoutManager = LinearLayoutManager(this)
+
 
         val client = AsyncHttpClient()
-
         client.get(NOW_PLAYING_URL,object: JsonHttpResponseHandler(){
             override fun onFailure(
                 statusCode: Int,
@@ -25,11 +42,21 @@ class MainActivity : AppCompatActivity() {
             ) {
                 Log.e(TAG,"onFailure $statusCode")
             }
-
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                 Log.i(TAG,"onSuccess:Json data $json")
-                val movieJsonArray = json.jsonObject.getJSONArray("results")
+                try{
+                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                    movies.addAll((Movie.fromJsonArray(movieJsonArray)))
+                    movieAdapter.notifyDataSetChanged()
+                    Log.i(TAG,"Movie list $movies")
+
+                }catch (e: JSONException){
+                    Log.e(TAG,"Encountered exception $e")
+                }
             }
-        }
+        })
+
     }
+
 }
+
